3323094e08feee266df5d713e95f358f93fca555
Adam Diehl <adadiehl@umich.edu>
2023-04-10 16:07:47 -0400

    Bump version: 1.1.0 → 1.1.1

--- utils/fitErrorModel.py
+++ utils/fitErrorModel.py
@@ -16,7 +16,7 @@ likelihood ratios for each error observation. Using the default bin step size of
 log-likelihood ratio distribution, just somewhat less granular.
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 
 
 def parse_histogram_file(log_file):
