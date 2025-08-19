a24e4d2e282280c8c30081c968f52f3861775a99
Adam Diehl <adadiehl@umich.edu>
2025-08-19 11:17:37 -0400

    Bump version: 1.1.12 → 1.1.13

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ def main():
 
     metadata = dict(
         name = 'HaplotagLR',
-        version = '1.1.12',
+        version = '1.1.13',
         license = 'MIT',
         description = 'Phasing individual long reads using known haplotype information.',
         description_content_type = 'text/plain',
--- src/HaplotagLR/__init__.py
+++ src/HaplotagLR/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '1.1.12'
+__version__ = '1.1.13'
 try:
     from ._HaplotagLR import longest  # noqa
 except ImportError:
